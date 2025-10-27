9260ad74983f6cf2ebef634b1662f0b533eb2072
jean-claude-segura <candidatspam001@laposte.net>
2025-10-27 12:53:10 +0100

    Writing straight to the stream for the xor
    uint_fast8_t instead of char

--- TPWMUnpacker/TPWMUnpacker.hpp
+++ TPWMUnpacker/TPWMUnpacker.hpp
@@ -13,9 +13,9 @@ All other byte sequences remain unchanged.
 In order to determine during unpacking where in the packed file an unpacking information is located instead of normal data bytes,
 the packer inserts an extra byte whose bits for the next 8 bytes indicate whether they are data bytes or unpacking information.
 */
-static int Unpack_TPWM(char* Input_Buffer, unsigned long packed_size, char* Output_Buffer, unsigned long unpacked_size)
+static int Unpack_TPWM(uint_fast8_t* Input_Buffer, unsigned long packed_size, uint_fast8_t* Output_Buffer, unsigned long unpacked_size)
 {
-    unsigned char b1, b2;
+    uint_fast8_t b1, b2;
     unsigned short packbyte;
     int bit, i;
     unsigned long input_offset = 0;
@@ -25,7 +25,7 @@ static int Unpack_TPWM(char* Input_Buffer, unsigned long packed_size, char* Outp
     while ((input_offset < packed_size) && (output_offset < unpacked_size))
     {
         // Lire l'octet de contrôle (flag byte)
-        packbyte = (unsigned char)Input_Buffer[input_offset++];
+        packbyte = (uint_fast8_t)Input_Buffer[input_offset++];
 
         for (bit = 0; bit < 8; bit++)
         {
@@ -38,8 +38,8 @@ static int Unpack_TPWM(char* Input_Buffer, unsigned long packed_size, char* Outp
                 if (input_offset + 1 >= packed_size)
                     return -1; // Pas assez de données pour lire b1 et b2
 
-                b1 = (unsigned char)Input_Buffer[input_offset++];
-                b2 = (unsigned char)Input_Buffer[input_offset++];
+                b1 = (uint_fast8_t)Input_Buffer[input_offset++];
+                b2 = (uint_fast8_t)Input_Buffer[input_offset++];
 
                 // Longueur = (b1 & 0x0F) + 2
                 length = (b1 & 0x0F) + 2;
@@ -78,7 +78,7 @@ static int Unpack_TPWM(char* Input_Buffer, unsigned long packed_size, char* Outp
 }
 
 // Utilitaire pour lire un entier 32 bits little-endian à partir d’un buffer
-static unsigned long read_le_uint32(const char* buf) {
+static unsigned long read_le_uint32(const uint_fast8_t* buf) {
     return ((unsigned char)buf[0]) |
         ((unsigned char)buf[1] << 8) |
         ((unsigned char)buf[2] << 16) |
@@ -94,10 +94,10 @@ static int Unpack_TPWMFile(const std::string strFileName, const std::string strE
     if (fsin.is_open())
     {
         // Lis tout le fichier dans un vecteur
-        std::vector<char> input_buffer((std::istreambuf_iterator<char>(fsin)), std::istreambuf_iterator<char>());
+        std::vector<uint_fast8_t> input_buffer((std::istreambuf_iterator<char>(fsin)), std::istreambuf_iterator<char>());
 
         // Files compressed with this packer can be easily recognized by an ID string "TPWM" directly at the beginning of the file.
-        if (strncmp(input_buffer.data(), "TPWM", 4) == 0)
+        if (strncmp((char*)input_buffer.data(), "TPWM", 4) == 0)
         {
             std::string outputfilename(strFileName);
             outputfilename += strExt;
@@ -108,7 +108,7 @@ static int Unpack_TPWMFile(const std::string strFileName, const std::string strE
                 // This ID string is followed by four bytes, which is the size of the unpacked data...
                 unsigned long unpacked_size = read_le_uint32(input_buffer.data() + 4);
 
-                auto output_buffer = std::make_unique<char[]>(unpacked_size);
+                auto output_buffer = std::make_unique<uint_fast8_t[]>(unpacked_size);
                 std::memset(output_buffer.get(), 0, unpacked_size);
 
                 // The size of the packed data thus results from the file size - 8
@@ -119,11 +119,15 @@ static int Unpack_TPWMFile(const std::string strFileName, const std::string strE
 
                 if (ret != -1)
                 {
-                    fsout.write(output_buffer.get(), unpacked_size);
+                    fsout.write(reinterpret_cast<char*>(output_buffer.get()), unpacked_size);
                     fsout.flush();
+                    fsout.close();
                 }
-
-                fsout.close();
+                else
+                {
+                    // Should delete the file here.
+                    fsout.close();
+                }
             }
         }
     }
--- XOR70/XOR70.hpp
+++ XOR70/XOR70.hpp
@@ -6,46 +6,48 @@
 #include <fstream>
 #include <vector>
 
-static int XOR70(char* Input_Buffer, unsigned long packed_size, char* Output_Buffer)
+static int XOR70(std::istream & fsin, std::ostream & fsout)
 {
-	auto tempin = Input_Buffer;
-	auto tempout = Output_Buffer;
+	// Lis tout le fichier dans un vecteur
+	std::vector<char> vin((std::istreambuf_iterator<char>(fsin)), std::istreambuf_iterator<char>());
+
+	char* tempin = vin.data();
+	auto filesize = vin.size();
 	const char sep_pattern[] = { 0x02, 0x01, 0x01, 0x00, 0x00 };
 	const char sep_pattern_multi[] = { 0x02, 0x02, 0x01, 0x00, 0x00 };
 	const char sep_pattern_split[] = { 0x02, 0x01, 0x00, 0x01, 0x00 };
 	const char sep_pattern_split_end[] = { 0x02, 0x02, 0x00, 0x00, 0x00 };
-	for (unsigned long i = 0; i < packed_size; ++i, ++tempin, ++tempout)
+	for (size_t i = 0; i < filesize; ++i, ++tempin)
 	{
 		if (strncmp(tempin, sep_pattern, 5) == 0)
 		{
-			strncpy(tempout, "[1]\r\n", 5);
+			fsout << "[1]" << std::endl;
 			i += 4;
 			tempin += 4;
-			tempout += 4;
 		}
 		else if (strncmp(tempin, sep_pattern_multi, 5) == 0)
 		{
-			strncpy(tempout, "[2]\r\n", 5);
+			fsout << "[2]" << std::endl;
 			i += 4;
 			tempin += 4;
-			tempout += 4;
 		}
 		else if (strncmp(tempin, sep_pattern_split, 5) == 0)
 		{
-			strncpy(tempout, "[3]\r\n", 5);
+			fsout << "[3]" << std::endl;
 			i += 4;
 			tempin += 4;
-			tempout += 4;
 		}
 		else if (strncmp(tempin, sep_pattern_split_end, 5) == 0)
 		{
-			strncpy(tempout, "[4]\r\n", 5);
+			fsout << "[4]" << std::endl;
 			i += 4;
 			tempin += 4;
-			tempout += 4;
 		}
 		else
-			*tempout = *tempin ^ 0x70;
+		{
+			char xored = *tempin ^ 0x70;
+			fsout.write(&xored, 1);
+		}
 	}
 
 	return 0; // Succès
@@ -60,28 +62,24 @@ int XOR70File(const std::string strFileName, const std::string strExt = ".x70")
 
 	if(fsin.is_open())
 	{
-		// Lis tout le fichier dans un vecteur
-		std::vector<char> input_buffer((std::istreambuf_iterator<char>(fsin)), std::istreambuf_iterator<char>());
-
 		std::string outputfilename(strFileName);
 		outputfilename += ".x70";
 		fsout.open(outputfilename, std::fstream::out | std::fstream::trunc | std::fstream::binary);
 
 		if (fsout.is_open())
-		{
-			const auto unpacked_size = input_buffer.size();
-
-			auto output_buffer = std::make_unique<char[]>(unpacked_size);
-			std::memset(output_buffer.get(), 0, unpacked_size);
-
-			ret = XOR70(input_buffer.data(), unpacked_size, output_buffer.get());
+		{			
+			ret = XOR70(fsin, fsout);
 
 			if (ret != -1)
 			{
-				fsout.write(output_buffer.get(), unpacked_size);
 				fsout.flush();
+				fsout.close();
 			}
-			fsout.close();
+			else
+			{
+				// Should delete the file here.
+				fsout.close();
+			}
 		}
 
 		fsin.close();
